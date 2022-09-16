836798e81ab7ef3eb0b1e881a63a157e677a5ed7
trungnt1270 <71129889+trungnt1270@users.noreply.github.com>
2022-09-16 21:25:28 +0700

    ASM_Buoi2_MVC

---
+++ ASM_Buoi2_MVC/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASM_Buoi2_MVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            TempData["Name"] = "Nguyen Hoai Nam";
+            TempData["Age"] = 28;
+            TempData["Address"] = "Ha Noi";
+            return View("ShowData");
+        }
+
+        public ActionResult ShowData()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASM_Buoi2_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASM_Buoi2_MVC.MvcApplication" Language="C#" %>
